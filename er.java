85d94f59d7ec00d4c66e2b0ebbcf43333b70d0c2
Varna Rithesh <varnarithu1704@gmail.com>
2025-11-25 15:11:26 +0530

    Update er.java

--- er.java
+++ er.java
@@ -1,3 +1,6 @@
-System.out.println("HIIII")
-System.out.println("VARNA")
-
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("HIIII");
+    System.out.println("VARNA");
+  }
+}
